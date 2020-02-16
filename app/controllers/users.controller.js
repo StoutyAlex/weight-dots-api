956a815ac76d82325748caa3f8382c4cdf877721
Alex Stout <alexstout009@hotmail.com>
2020-02-16 00:20:57 +0000

    created controller

---
+++ app/controllers/users.controller.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+const { success, error } = require('../helpers/response');
+const userService = require('../services/users.service');
+
+router.get('/', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const newUser = await userService.login({ username, password });
+    return success(res, newUser);
+  } catch (err) {
+    return error(res, err)
+  }
+});
+
+router.post('/', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    await userService.create({ username, password });
+    const loginUser = await userService.login({ username, password });
+    return success(res, loginUser);
+  } catch (err) {
+    error(res, err)
+  }
+});
+
+module.exports = router;
